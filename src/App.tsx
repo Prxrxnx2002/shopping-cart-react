781b33acbe3b43c91b231673c2a0648577ed2920
Prerana <porechaprerana@gmail.com>
2024-02-28 10:49:43 +0530

    fetch products

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+import { useQuery } from "react-query";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  title: string;
+  price: number;
+  image: string;
+  description: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
+
 function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
+
   return (
     <div className="App">
       Shopping Cart.
